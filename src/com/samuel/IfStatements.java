120288e201678831f40acdcc2a6af354ab9089b5
samuelmutinda <mutindasamuel01@gmail.com>
2022-07-12 21:46:06 +0300

    If statements

--- src/com/samuel/IfStatements.java
+++ src/com/samuel/IfStatements.java
@@ -1,4 +1,16 @@
 package com.samuel;
 
 public class IfStatements {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age>=18){
+            System.out.println("You are an adult");
+        }
+        else if (age>=13 && age<18){
+            System.out.println("You are an teenager");
+        }
+        else {
+            System.out.println("You are a child");
+        }
+    }
 }
