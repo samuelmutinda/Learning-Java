257a54b9f14ec4e37ee9764b3d36e382e54659f3
samuelmutinda <mutindasamuel01@gmail.com>
2022-07-12 22:05:47 +0300

    Switch statements

--- src/com/samuel/SwitchStatements.java
+++ src/com/samuel/SwitchStatements.java
@@ -2,6 +2,21 @@ package com.samuel;
 
 public class SwitchStatements {
     public static void main(String[] args) {
+        String gender = "FEMALE";
+        //=> Let us check if gender is male, female or prefer not to say
+        switch (gender){
+            case "FEMALE":
+                System.out.println("You are a female");
+                break;
+            case "MALE":
+                System.out.println("You are a male");
+                break;
+            case "PREFER NOT TO SAY":
+                System.out.println("Prefer not to say");
+                break;
+            default:
+                System.out.println("Unknown gender");
+        }
 
     }
 }
