a4da2ada15e7fcbbe694fef20499266e6eb8d3b1
samuelmutinda <mutindasamuel01@gmail.com>
2022-07-14 13:25:29 +0300

    More arrays in Java(++ and --operators)

--- src/com/samuel/ArraysInJava.java
+++ src/com/samuel/ArraysInJava.java
@@ -26,5 +26,23 @@ public class ArraysInJava {
         System.out.println(Arrays.toString(animals));
         //=> We can find the size of an array
         System.out.println("The size of animals array is "+animals.length);
+
+        //=> In order to access elements in an array, mention its index:
+        System.out.println("The element in index 2 of animals is "+numbers[2]);
+
+        //=> Increment(++) and Decrement(--)
+        int number = 1;
+        number++;
+        System.out.println("number was 1. It is now "+number);
+        number = 3;
+        number--;
+        System.out.println("number was 3. It is now "+number);
+
+        //=> Similarly, we can do += and -+
+        number = 5;
+        number += 1;
+        System.out.println("Number was 5. It is now "+number);
+        number -= 1;
+        System.out.println("Number was 6. It is now "+number);
     }
 }
---
+++ src/com/samuel/LoopsInJava.java
@@ -0,0 +1,4 @@
+package com.samuel;
+
+public class LoopsInJava {
+}
